0f96ca5c10cfce12e7ea48342faf8346b9ce09fd
Xiaohua <cheonghiuwaa@gmail.com>
2018-12-26 22:18:48 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int result = 0;
+        int i = 0;
+        while (i < 10) {
+            result += i;
+            System.out.println(result + " ");
+            i += 1;
+        }
+    }   
+}
